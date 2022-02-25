06992cc952002c04e19cf6d7a16965e83fc28f12
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-02-25 14:55:12 +0530

    Create MySQLNotes.sql

---
+++ MySQLNotes.sql
@@ -0,0 +1,16 @@
+### Start the CLI (Command Line Interface)
+
+mysql-ctl cli
+
+### Showing all the available databases
+
+show databases;
+
+### Creating New Database Basic Command
+
+CREATE DATABASE <name>
+
+-- Example
+
+CREATE DATABASE soap_store;
+
