bfbb2f3775891b1097ff46374e6962e7ac7072be
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-02-26 10:57:52 +0530

    Update MySQLNotes.sql

--- MySQLNotes.sql
+++ MySQLNotes.sql
@@ -1,16 +1,45 @@
-### Start the CLI (Command Line Interface)
-
+-- Start the CLI (Command Line Interface)
 mysql-ctl cli
 
-### Showing all the available databases
-
+-- Showing all the available databases
 show databases;
 
-### Creating New Database Basic Command
-
-CREATE DATABASE <name>
+-- Creating New Database Basic Command
+CREATE DATABASE database_name;
 
 -- Example
-
 CREATE DATABASE soap_store;
 
+-- To drop a Database
+DROP DATABASE database_name;
+
+-- Example
+DROP DATABASE hello_world_db;
+
+-- To Use a Database
+USE database_name;
+
+-- Example
+USE dog_walking_app;
+
+-- To see which database we are using
+SELECT database();
+
+-- What are Tables?
+-- Tables are a collection of related data held in a structural format within a databse;
+
+-- Creating a New Table
+CREATE TABLE tablename
+  (
+    column_name data_type,
+    column_name data_type
+  );
+
+-- Example
+CREATE TABLE cats
+  (
+    name VARCHAR(100),
+    age INT
+  );
+  
+  
