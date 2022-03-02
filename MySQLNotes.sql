efc26b6a58f2ca6b35f3b63f221ba4aa3652f087
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-03-02 21:02:37 +0530

    Update MySQLNotes.sql

--- MySQLNotes.sql
+++ MySQLNotes.sql
@@ -350,3 +350,35 @@ SELECT REVERSE(author_fname) FROM books;
 SELECT CONCAT('woof', REVERSE('woof'));
 SELECT CONCAT(author_fname, REVERSE(author_fname)) FROM books;
 
+
+-- 🟢Working with CHAR_LENGTH
+SELECT CHAR_LENGTH('Hello World');
+SELECT author_lname, CHAR_LENGTH(author_lname) AS 'length' FROM books;
+SELECT CONCAT(author_lname, ' is ', CHAR_LENGTH(author_lname), ' characters long') FROM books;
+
+
+-- 🟢Changing case with UPPER and LOWER
+SELECT UPPER('Hello World');
+SELECT LOWER('Hello World');
+SELECT UPPER(title) FROM books;
+SELECT CONCAT('MY FAVORITE BOOK IS ', UPPER(title)) FROM books;
+SELECT CONCAT('MY FAVORITE BOOK IS ', LOWER(title)) FROM books;
+
+
+-- 🟢The order is important when dealing with combining/wrapping certain string functions.
+-- For example:
+-- This works:
+SELECT UPPER(CONCAT(author_fname, ' ', author_lname)) AS "full name in caps"
+FROM books;
+
+-- While this does not:
+SELECT CONCAT(UPPER(author_fname, ' ', author_lname)) AS "full name in caps" 
+FROM books;
+
+-- UPPER only takes one argument and CONCAT takes two or more arguments, so they can't be switched in that way.
+-- You could do it this way, however:
+SELECT CONCAT(UPPER(author_fname), ' ', UPPER(author_lname)) AS "full name in caps" 
+FROM books;
+
+-- But, the first example above would be better (more DRY*) because you wouldn't need to call UPPER two times.
+
