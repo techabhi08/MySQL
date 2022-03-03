8d6d08a86e2fb2a44fb65d9e38ae1af2331b6dbc
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-03-03 14:08:07 +0530

    Update MySQLNotes.sql

--- MySQLNotes.sql
+++ MySQLNotes.sql
@@ -382,3 +382,57 @@ FROM books;
 
 -- But, the first example above would be better (more DRY*) because you wouldn't need to call UPPER two times.
 
+
+-- 🟢Refining our selections.
+-- Adding some new books
+INSERT INTO books
+    (title, author_fname, author_lname, released_year, stock_quantity, pages)
+    VALUES ('10% Happier', 'Dan', 'Harris', 2014, 29, 256), 
+           ('fake_book', 'Freida', 'Harris', 2001, 287, 428),
+           ('Lincoln In The Bardo', 'George', 'Saunders', 2017, 1000, 367);
+	  
+
+-- 🟢Using Distinct
+SELECT author_lname FROM books;
+SELECT DISTINCT author_lname FROM books;
+
+SELECT author_fname, author_lname FROM books;
+
+SELECT DISTINCT CONCAT(author_fname,' ', author_lname) FROM books;
+SELECT DISTINCT author_fname, author_lname FROM books; -- THis is same as the above query
+
+
+-- 🟢Sorting data with ORDER BY
+-- Basic syntax with exampls:
+SELECT author_lname FROM books ORDER BY author_lname;
+
+SELECT title FROM books ORDER BY title;
+SELECT author_lname FROM books ORDER BY author_lname DESC; --Printing in DESC ORDER as BY DEFAULT is ASCENDING
+
+SELECT released_year FROM books ORDER BY released_year;
+SELECT released_year FROM books ORDER BY released_year DESC;
+SELECT released_year FROM books ORDER BY released_year ASC;
+ 
+SELECT title, released_year, pages FROM books ORDER BY released_year;
+SELECT title, pages FROM books ORDER BY released_year;
+
+
+SELECT title, author_fname, author_lname 
+FROM books ORDER BY 2;
+ 
+SELECT title, author_fname, author_lname 
+FROM books ORDER BY 3;
+ 
+SELECT title, author_fname, author_lname 
+FROM books ORDER BY 1;
+ 
+SELECT title, author_fname, author_lname 
+FROM books ORDER BY 1 DESC;
+ 
+SELECT author_lname, title
+FROM books ORDER BY 2;
+
+SELECT author_fname, author_lname FROM books 
+ORDER BY author_lname, author_fname;
+
+
