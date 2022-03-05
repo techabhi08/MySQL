1ec6826780d0f68f5df6a7f269dd60da30b58756
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-03-05 16:36:50 +0530

    Update MySQLNotes.sql

--- MySQLNotes.sql
+++ MySQLNotes.sql
@@ -509,11 +509,53 @@ SELECT CONCAT('In ', released_year, ' ', COUNT(*), ' book(s) released') AS year 
 
 
 
+-- 🟢MIN and MAX Basics
+
+SELECT MIN(released_year) FROM books;
+ 
+SELECT MIN(pages) FROM books;
+SELECT MAX(pages) FROM books;
+ 
+SELECT MAX(released_year) FROM books;
+SELECT MAX(pages), title FROM books; -- This WILL NOT give the title of book with max pages.
+
+-- To find the TITLE of book with MIN or MAX pages we can do this instead of the one written above
+SELECT * FROM books WHERE pages = (SELECT Min(pages) FROM books); 
+SELECT title, pages FROM books WHERE pages = (SELECT Max(pages) FROM books); 
+SELECT title, pages FROM books WHERE pages = (SELECT Min(pages) FROM books); 
+ 
+ -- OR can do this
+SELECT * FROM books ORDER BY pages ASC LIMIT 1;
+SELECT title, pages FROM books ORDER BY pages ASC LIMIT 1;
+SELECT * FROM books ORDER BY pages DESC LIMIT 1;
 
 
 
+-- 🟢USING MIN and MAX with GROUP BY
+
+SELECT author_fname, author_lname, Min(released_year) FROM books GROUP  BY author_lname, author_fname;
+SELECT author_fname, author_lname,Max(pages) FROM books GROUP BY author_lname, author_fname;
+SELECT CONCAT(author_fname, ' ', author_lname) AS author, MAX(pages) AS 'longest book' FROM books GROUP BY author_lname, author_fname;
 
 
 
+-- 🟢The SUM Function
+SELECT SUM(pages) FROM books;
+SELECT SUM(released_year) FROM books;
+ 
+SELECT author_fname, author_lname, Sum(pages) FROM books GROUP BY author_lname, author_fname;
+SELECT author_fname, author_lname, Sum(released_year) FROM books GROUP BY author_lname, author_fname;
+
+
+
+-- 🟢The AVG Function
+SELECT AVG(released_year) FROM books;
+ 
+SELECT AVG(pages) FROM books;
+ 
+SELECT AVG(stock_quantity) FROM books GROUP BY released_year;
+ 
+SELECT released_year, AVG(stock_quantity) FROM books GROUP BY released_year;
+SELECT author_fname, author_lname, AVG(pages) FROM booksGROUP BY author_lname, author_fname;
 
 
