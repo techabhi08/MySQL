057d6e61b3550e0b368e5f8df75218a8f1bdfbc5
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-03-05 14:28:09 +0530

    Update MySQLNotes.sql

--- MySQLNotes.sql
+++ MySQLNotes.sql
@@ -475,7 +475,36 @@ SELECT title FROM books WHERE title LIKE '%\_%'
 
 
 
+-- 🟢The COUNT Function
+SELECT COUNT(*) FROM books;
+SELECT COUNT(author_fname) FROM books;
 
+SELECT COUNT(DISTINCT author_fname) FROM books; 
+SELECT COUNT(DISTINCT author_lname) FROM books;
+SELECT COUNT(DISTINCT author_lname, author_fname) FROM books;
+
+SELECT title FROM books WHERE title LIKE '%the%'; 
+SELECT COUNT(*) FROM books WHERE title LIKE '%the%';
+
+
+-- 🟢The GROUP BY Function
+
+-- Group By summarizes or aggregates the identical data into a single row.
+SELECT title, author_lname FROM books;
+ 
+SELECT title, author_lname FROM books GROUP BY author_lname;
+
+SELECT author_lname, COUNT(*) FROM books GROUP BY author_lname;
+ 
+ 
+SELECT title, author_fname, author_lname FROM books; 
+SELECT title, author_fname, author_lname FROM books GROUP BY author_lname;
+SELECT author_fname, author_lname, COUNT(*) FROM books GROUP BY author_lname;
+SELECT author_fname, author_lname, COUNT(*) FROM books GROUP BY author_lname, author_fname;
+ 
+SELECT released_year FROM books;
+SELECT released_year, COUNT(*) FROM books GROUP BY released_year;
+SELECT CONCAT('In ', released_year, ' ', COUNT(*), ' book(s) released') AS year FROM books GROUP BY released_year;
 
 
 
