588069a379de0df145a18c8e041e54f9ce0c9e9b
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-03-15 21:50:01 +0530

    Update MySQLNotes.sql

--- MySQLNotes.sql
+++ MySQLNotes.sql
@@ -595,7 +595,16 @@ INSERT INTO thingies(price) VALUES (8877665544.45);
 SELECT * FROM thingies;
 
 
-
+-- 🟢Creating the DATE data
+CREATE TABLE people (name VARCHAR(100), birthdate DATE, birthtime TIME, birthdt DATETIME);
+ 
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES('Padma', '1983-11-11', '10:07:35', '1983-11-11 10:07:35');
+ 
+INSERT INTO people (name, birthdate, birthtime, birthdt)
+VALUES('Larry', '1943-12-25', '04:10:42', '1943-12-25 04:10:42');
+ 
+SELECT * FROM people;
 
 
 
