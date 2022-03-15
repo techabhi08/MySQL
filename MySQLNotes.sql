76256e6dff4088b9140ae511bcab35a4ac35a64d
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-03-15 17:23:20 +0530

    Update MySQLNotes.sql

--- MySQLNotes.sql
+++ MySQLNotes.sql
@@ -559,3 +559,42 @@ SELECT released_year, AVG(stock_quantity) FROM books GROUP BY released_year;
 SELECT author_fname, author_lname, AVG(pages) FROM booksGROUP BY author_lname, author_fname;
 
 
+-- 🟢Revisiting Data Types
+
+-- 🟢CHAR and VARCHAR
+CREATE TABLE dogs (name CHAR(5), breed VARCHAR(10));
+INSERT INTO dogs (name, breed) VALUES ('bob', 'beagle');
+INSERT INTO dogs (name, breed) VALUES ('robby', 'corgi');
+INSERT INTO dogs (name, breed) VALUES ('Princess Jane', 'Retriever');
+SELECT * FROM dogs;
+ 
+INSERT INTO dogs (name, breed) VALUES ('Princess Jane', 'Retrievesadfdsafdasfsafr');
+SELECT * FROM dogs;
+
+
+-- 🟢DECIMAL
+
+CREATE TABLE items(price DECIMAL(5,2)); 
+INSERT INTO items(price) VALUES(7);
+INSERT INTO items(price) VALUES(7987654);
+INSERT INTO items(price) VALUES(34.88);
+INSERT INTO items(price) VALUES(298.9999);
+INSERT INTO items(price) VALUES(1.9999);
+ 
+SELECT * FROM items;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
