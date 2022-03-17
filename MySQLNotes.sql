69657701c3b99a190e09afddcb03c7bf8d334dbf
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-03-17 17:27:28 +0530

    Update MySQLNotes.sql

--- MySQLNotes.sql
+++ MySQLNotes.sql
@@ -787,6 +787,83 @@ WHERE
     AND CAST('2000-01-01' AS DATETIME);
     
     
+-- 🟢IN and NOT IN
+    
+    show databases();
+use book_shop;
+ 
+SELECT 
+    title, 
+    author_lname 
+FROM books
+WHERE author_lname='Carver' OR
+      author_lname='Lahiri' OR
+      author_lname='Smith';
+ 
+SELECT title, author_lname FROM books WHERE author_lname IN ('Carver', 'Lahiri', 'Smith');
+SELECT title, released_year FROM books WHERE released_year IN (2017, 1985);
+ 
+SELECT title, released_year FROM books
+WHERE released_year != 2000 AND
+      released_year != 2002 AND
+      released_year != 2004 AND
+      released_year != 2006 AND
+      released_year != 2008 AND
+      released_year != 2010 AND
+      released_year != 2012 AND
+      released_year != 2014 AND
+      released_year != 2016;
+ 
+SELECT title, released_year FROM books WHERE released_year NOT IN (2000,2002,2004,2006,2008,2010,2012,2014,2016);
+SELECT title, released_year FROM books WHERE released_year >= 2000 AND released_year NOT IN (2000,2002,2004,2006,2008,2010,2012,2014,2016);
+SELECT title, released_year FROM books WHERE released_year >= 2000 AND released_year % 2 != 0;
+SELECT title, released_year FROM books WHERE released_year >= 2000 AND released_year % 2 != 0 ORDER BY released_year;
     
     
-    
+ 
+ -- 🟢CASE Statements
+ 
+ SELECT title, released_year,
+       CASE 
+         WHEN released_year >= 2000 THEN 'Modern Lit'
+         ELSE '20th Century Lit'
+       END AS GENRE
+FROM books;
+ 
+SELECT title, stock_quantity,
+    CASE 
+        WHEN stock_quantity BETWEEN 0 AND 50 THEN '*'
+        WHEN stock_quantity BETWEEN 51 AND 100 THEN '**'
+        ELSE '***'
+    END AS STOCK
+FROM books;
+ 
+SELECT title,
+    CASE 
+        WHEN stock_quantity BETWEEN 0 AND 50 THEN '*'
+        WHEN stock_quantity BETWEEN 51 AND 100 THEN '**'
+        ELSE '***'
+    END AS STOCK
+FROM books;
+ 
+SELECT title, stock_quantity,
+    CASE 
+        WHEN stock_quantity BETWEEN 0 AND 50 THEN '*'
+        WHEN stock_quantity BETWEEN 51 AND 100 THEN '**'
+        WHEN stock_quantity BETWEEN 101 AND 150 THEN '***'
+        ELSE '****'
+    END AS STOCK
+FROM books;
+ 
+SELECT title, stock_quantity,
+    CASE 
+        WHEN stock_quantity <= 50 THEN '*'
+        WHEN stock_quantity <= 100 THEN '**'
+        ELSE '***'
+    END AS STOCK
+FROM books; 
+
+
+
+
+
