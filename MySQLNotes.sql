38690bb437655773ccd6382d97cda4c631ff911a
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-03-17 11:17:45 +0530

    Update MySQLNotes.sql

--- MySQLNotes.sql
+++ MySQLNotes.sql
@@ -766,4 +766,27 @@ WHERE  author_lname = 'Eggers'
 OR     stock_quantity > 100;
 
 
+-- 🟢BETWEEN
 
+SELECT title, released_year FROM books WHERE released_year >= 2004 && released_year <= 2015;
+SELECT title, released_year FROM books WHERE released_year BETWEEN 2004 AND 2015;
+SELECT title, released_year FROM books WHERE released_year NOT BETWEEN 2004 AND 2015;
+ 
+SELECT CAST('2017-05-02' AS DATETIME);
+ 
+show databases;
+use new_testing_db;
+SELECT name, birthdt FROM people WHERE birthdt BETWEEN '1980-01-01' AND '2000-01-01';
+ 
+SELECT 
+    name, 
+    birthdt 
+FROM people
+WHERE 
+    birthdt BETWEEN CAST('1980-01-01' AS DATETIME)
+    AND CAST('2000-01-01' AS DATETIME);
+    
+    
+    
+    
+    
