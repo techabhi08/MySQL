c87ba5ac8be19db599dd1bc1cbc5fc22bf52f5a8
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-03-17 10:37:12 +0530

    Update MySQLNotes.sql

--- MySQLNotes.sql
+++ MySQLNotes.sql
@@ -704,5 +704,16 @@ SELECT title, author_lname FROM books WHERE author_lname = 'eGGers';
 
 
 
+-- 🟢LESS THAN
 
+SELECT title, released_year FROM books;
+ 
+SELECT title, released_year FROM books WHERE released_year < 2000;
+SELECT title, released_year FROM books WHERE released_year <= 2000;
+ 
+SELECT 3 < -10; -- false
+SELECT -10 < -9; -- true
+SELECT 42 <= 42; -- true
+SELECT 'h' < 'p'; -- true
+SELECT 'Q' <= 'q'; -- true
 
