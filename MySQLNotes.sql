0569fdf5bec14c46fc78193808abf6bd9bac1f30
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-03-22 17:17:02 +0530

    Update MySQLNotes.sql

--- MySQLNotes.sql
+++ MySQLNotes.sql
@@ -1123,3 +1123,33 @@ SELECT
 FROM series
 JOIN reviews
     ON series.id = reviews.series_id;
+    
+-- 🟢Challenge 2 AVG rating
+
+SELECT
+    title,
+    AVG(rating) as avg_rating
+FROM series
+JOIN reviews
+    ON series.id = reviews.series_id
+GROUP BY series.id
+ORDER BY avg_rating;
+
+-- 🟢CHALLENGE 3  - Two Solutions
+
+SELECT
+    first_name,
+    last_name,
+    rating
+FROM reviewers
+INNER JOIN reviews
+    ON reviewers.id = reviews.reviewer_id;
+    
+
+SELECT
+    first_name,
+    last_name,
+    rating
+FROM reviews
+INNER JOIN reviewers
+    ON reviewers.id = reviews.reviewer_id;
