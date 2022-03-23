72d570b4393d1d220c09d89db39536b09ab42f21
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-03-23 19:37:11 +0530

    Update MySQLNotes.sql

--- MySQLNotes.sql
+++ MySQLNotes.sql
@@ -1247,3 +1247,40 @@ CREATE TABLE comments (
     FOREIGN KEY(photo_id) REFERENCES photos(id),
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
+
+-- 🟢IG Clone LIKES Schema
+
+CREATE TABLE likes (
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    PRIMARY KEY(user_id, photo_id)
+);
+
+-- 🟢IG Clone FOLLOWS Schema
+
+CREATE TABLE follows(
+    follower_id INTEGER NOT NULL,
+    followee_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(follower_id) REFERENCES users(id),
+    FOREIGN KEY(followee_id) REFERENCES users(id),
+    PRIMARY KEY(follower_id, followee_id)
+);
+
+-- 🟢IG Clone Hashtags Schema
+
+CREATE TABLE tags (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  tag_name VARCHAR(255) UNIQUE,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+CREATE TABLE photo_tags (
+    photo_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL,
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(tag_id) REFERENCES tags(id),
+    PRIMARY KEY(photo_id, tag_id)
+);
