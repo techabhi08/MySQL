69e5e8c757a96320be8bb3f62c80bf7a39a01e66
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-03-26 18:00:55 +0530

    Update MySQLNotes.sql

--- MySQLNotes.sql
+++ MySQLNotes.sql
@@ -1528,3 +1528,49 @@ connection.query(q, [data], function(err, result) {
  
 connection.end();
 
+
+-- 🟢Our First Simple Web App:
+-- 🟢Add to your app.js file:
+
+var express = require('express');
+ 
+var app = express();
+ 
+app.get("/", function(req, res){
+ res.send("HELLO FROM OUR WEB APP!");
+});
+ 
+app.listen(8080, function () {
+ console.log('App listening on port 8080!');
+});
+
+-- 🟢Remember to start the server up:
+
+node app.js
+
+-- 🟢Adding Multiple Routes
+-- 🟢Add a /joke route:
+
+app.get("/joke", function(req, res){
+ var joke = "What do you call a dog that does magic tricks? A labracadabrador.";
+ res.send(joke);
+});
+
+-- 🟢Add a /random_num route:
+
+app.get("/random_num", function(req, res){
+ var num = Math.floor((Math.random() * 10) + 1);
+ res.send("Your lucky number is " + num);
+});
+
+-- 🟢Connecting Express and MySQL
+-- 🟢Add the MySQL code inside of the root route:
+
+app.get("/", function(req, res){
+ var q = 'SELECT COUNT(*) as count FROM users';
+ connection.query(q, function (error, results) {
+ if (error) throw error;
+ var msg = "We have " + results[0].count + " users";
+ res.send(msg);
+ });
+});
