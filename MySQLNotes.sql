84af7001898339fc5052eb669faf54e981d68a23
techabhi08 <75834097+techabhi08@users.noreply.github.com>
2022-03-26 13:22:35 +0530

    Update MySQLNotes.sql

--- MySQLNotes.sql
+++ MySQLNotes.sql
@@ -1498,3 +1498,33 @@ var end_result = connection.query('INSERT INTO users SET ?', person, function(er
   console.log(result);
 });
 
+-- The Code To INSERT 500 Random Users
+var mysql = require('mysql');
+var faker = require('faker');
+ 
+ 
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  database : 'join_us'
+});
+ 
+ 
+var data = [];
+for(var i = 0; i < 500; i++){
+    data.push([
+        faker.internet.email(),
+        faker.date.past()
+    ]);
+}
+ 
+ 
+var q = 'INSERT INTO users (email, created_at) VALUES ?';
+ 
+connection.query(q, [data], function(err, result) {
+  console.log(err);
+  console.log(result);
+});
+ 
+connection.end();
+
